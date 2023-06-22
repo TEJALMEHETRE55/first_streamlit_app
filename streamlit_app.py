473227de9167ef74efe474760dd67189e2768552
TEJALMEHETRE55 <137394835+TEJALMEHETRE55@users.noreply.github.com>
2023-06-22 16:11:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("my parents")
+streamlit.title("my parents healthy diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("omega and  blueberry oatmeal") 
+streamlit.text("kale,pinach and rocket smoothi") 
+streamlit.text("Hard boiled free-range Egg") 
